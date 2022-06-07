0d9878296052923e721ed5f21d96ad92195061f9
Dimitri <99058899@mydavinci.nl>
2022-06-07 12:41:01 +0200

    6

---
+++ coins/coins.py
@@ -0,0 +1,41 @@
+# name of student:  Dimitri Aistov
+# number of student:  99058899
+# purpose of program: wisselgeld berekenen
+# function of program:  wisselgeld berekenen
+# structure of program: python
+
+toPay = int(float(input('Amount to pay: '))* 100) #
+paid = int(float(input('Paid amount: ')) * 100) #
+change = paid - toPay #
+
+if change > 0: #
+  coinValue = 50 #
+  
+  while change > 0 and coinValue > 0: #
+    nrCoins = change // coinValue #
+
+    if nrCoins > 0: #
+      print('return maximal ', nrCoins, ' coins of ', coinValue, ' cents!' ) #
+      nrCoinsReturned = int(input('How many coins of ' + str(coinValue) +  ' cents did you return? ')) #
+      change -= nrCoinsReturned * coinValue #
+
+# comment on code below: 
+    if coinValue == 50:
+      coinValue = 20
+    elif coinValue == 20:
+      coinValue = 10
+    elif coinValue == 10:
+      coinValue = 5
+    elif coinValue == 5:
+      coinValue = 3
+    elif coinValue == 3:
+      coinValue = 2
+    elif coinValue == 2:
+      coinValue = 1
+    else:
+      coinValue = 0
+
+if change > 0: #
+  print('Change not returned: ', str(change) + ' cents')
+else:
+  print('done')
